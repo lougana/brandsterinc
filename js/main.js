29de9bddceef24ed951539f307281f07392cffad
Luis Magana <lmaganmbox@gmail.com>
2016-01-25 15:47:26 -0500

    Added Auto Scroll to Nav and Resize Logo Images

--- js/main.js
+++ js/main.js
@@ -23,6 +23,26 @@ ga('send', 'pageview');
 $(document).ready(function (e) {
 
     /* -------------------------------------
+            Anchor Scroll Start
+    --------------------------------------- */
+
+    $('.anchor-scroll').anchorScroll({
+       scrollSpeed: 800, // scroll speed
+       offsetTop: 0, // offset for fixed top bars (defaults to 0)
+       onScroll: function () {
+         // callback on scroll start
+       },
+       scrollEnd: function () {
+         // callback on scroll end
+       }
+    });
+
+    /* -------------------------------------
+            Anchor Scroll End
+    --------------------------------------- */
+
+
+    /* -------------------------------------
      HOME SLIDER
      -------------------------------------- */
     $("#home-slider").owlCarousel({
@@ -222,7 +242,7 @@ $(document).ready(function (e) {
     });
 ////	 -------------------------------------
 //			NAVIGATION
-//	 -------------------------------------- 
+//	 --------------------------------------
     /* ---------------------------------------
      PRELOADER
      --------------------------------------- */
