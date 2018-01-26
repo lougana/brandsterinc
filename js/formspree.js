04727f904e9c4a1c4b1802f032e47d03658a5f0c
Luis Magana <luis@Luiss-MacBook-Pro.local>
2018-01-26 11:28:25 -0500

    Added support for static form from formspree

---
+++ js/formspree.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+
+    $('#storeForm').on('submit', function(e) {
+        e.preventDefault();
+        
+        //get the name field value
+        var name = $('#name').val();
+        //get the name field value
+        var email = $('#email').val();
+        //get the comments
+        var number = $('#number').val();
+                    
+        //pretend we don't need validation
+        
+        //send to formspree
+        $.ajax({
+            url:'https://formspree.io/systems@brandster.com',
+            method:'POST',
+            data:{
+                name:name,
+                _replyto:email,
+                 email:email,
+                number:number,
+                _subject:'Brandster Store Customer Contact',
+            },
+            dataType:"json",
+            success:function() {
+                console.log('success'); 
+                $('#formBlock').hide();
+                $('#thankyouBlock').show();
+            }   
+
+        });     
+        
+    });
+
+}); 
